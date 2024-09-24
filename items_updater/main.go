1998ebeb5c2f65269c9bed420c17cfa188c10d5c
mkulik05 <mishakulik2@gmail.com>
2024-09-24 18:05:44 +0300

    added kafka listener to update items info, add new item, remove

---
+++ items_updater/main.go
@@ -0,0 +1,83 @@
+	package main
+
+	import (
+		"context"
+		"encoding/json"
+		"fmt"
+		"log"
+		"os"
+
+		"github.com/jackc/pgx/v5/pgxpool"
+		"github.com/segmentio/kafka-go"
+	)
+
+	type ItemInfo struct {
+		ItemID          int64  `json:"item_id"`
+		ItemName        string `json:"item_name"`
+		ItemAmount      int32  `json:"item_amount"`
+		ItemPrice       int64  `json:"item_price"`
+		ItemDescription string `json:"item_description"`
+		TimesBought     int    `json:"times_bought"`
+	}
+
+	type ItemMessage struct {
+		Action string   `json:"action"` // "add", "modify", "delete"
+		Item   ItemInfo `json:"item"`
+	}
+
+	func handleMessage(dbpool *pgxpool.Pool, msg ItemMessage) error {
+		switch msg.Action {
+		case "add":
+			_, err := dbpool.Exec(context.Background(), "INSERT INTO items (item_name, item_amount, item_price, item_description, times_bought) VALUES ($1, $2, $3, $4, $5)",
+				msg.Item.ItemName, msg.Item.ItemAmount, msg.Item.ItemPrice, msg.Item.ItemDescription, msg.Item.TimesBought)
+			return err
+		case "modify":
+			_, err := dbpool.Exec(context.Background(), "UPDATE items SET item_name=$1, item_amount=$2, item_price=$3, item_description=$4, times_bought=$5 WHERE item_id=$6",
+				msg.Item.ItemName, msg.Item.ItemAmount, msg.Item.ItemPrice, msg.Item.ItemDescription, msg.Item.TimesBought, msg.Item.ItemID)
+			return err
+		case "delete":
+			_, err := dbpool.Exec(context.Background(), "DELETE FROM items WHERE item_id=$1", msg.Item.ItemID)
+			return err
+		default:
+			return fmt.Errorf("unknown action: %s", msg.Action)
+		}
+	}
+
+	func main() {
+
+		dbpool, err := pgxpool.New(context.Background(), os.Getenv("DATABASE_URI"))
+		if err != nil {
+			log.Fatalf("Failed to connect to database: %v", err)
+		}
+		defer dbpool.Close()
+
+
+		r := kafka.NewReader(kafka.ReaderConfig{
+			Brokers:   []string{os.Getenv("KAFKA_URL")},
+			Topic:     "items-updates",
+			Partition: 0,
+			MaxBytes:  10e6, 
+		})
+
+		defer r.Close()
+
+		for {
+			m, err := r.ReadMessage(context.Background())
+			if err != nil {
+				log.Printf("Error reading message: %v", err)
+				break
+			}
+
+			var itemMsg ItemMessage
+			if err := json.Unmarshal(m.Value, &itemMsg); err != nil {
+				log.Printf("Error unmarshalling message: %v", err)
+				continue
+			}
+
+			if err := handleMessage(dbpool, itemMsg); err != nil {
+				log.Printf("Error handling message: %v", err)
+			} else {
+				log.Printf("Successfully processed message: %s", string(m.Value))
+			}
+		}
+	}
