c4455e9eff466528409c4adb11d503160cb07058
mkulik05 <mishakulik2@gmail.com>
2024-10-06 13:44:47 +0300

    small restructure

--- load_testing/locust-file.py
+++ load_testing/locust-file.py
@@ -6,15 +6,14 @@ class WebsiteUser(HttpUser):
     wait_time = between(5, 15)
 
     def on_start(self):
-        # Register a new user and log in to obtain JWT
+       
         self.jwt = self.register_and_login_user()
         
     def register_and_login_user(self):
-        # Generate a unique login
-        login = f"tuser{self.user_id()}-{random.randint(31, 9000000000)}"
-        pwd = "password123"
 
-        # Register the user
+        login = f"tuser{self.environment.runner.user_count}-{random.randint(31, 9000000000)}"
+        pwd = f"password123-{random.randint(31, 9000000000)}"
+
         register_url = "/auth/register"
         headers = {
             "Content-Type": "application/json"
@@ -25,7 +24,6 @@ class WebsiteUser(HttpUser):
         }
         response = self.client.post(register_url, headers=headers, data=json.dumps(register_data))
 
-        # Log in the user to get JWT
         login_url = "/auth/login"
         login_data = {
             "login": login,
@@ -33,7 +31,7 @@ class WebsiteUser(HttpUser):
         }
         response = self.client.post(login_url, headers=headers, data=json.dumps(login_data))
 
-        # Assuming the JWT is returned in the response JSON under the key 'token'
+
         return response.json().get('token')
 
     @task
@@ -41,7 +39,7 @@ class WebsiteUser(HttpUser):
         url = "/orders/order"
         headers = {
             "Content-Type": "application/json",
-            "Authorization": self.jwt  # Use the JWT for authorization
+            "Authorization": self.jwt   for authorization
         }
         data = {
             "items": {
@@ -58,7 +56,7 @@ class WebsiteUser(HttpUser):
     def get_orders(self):
         url = "/orders/list"
         headers = {
-            "Authorization": self.jwt  # Use the JWT for authorization
+            "Authorization": self.jwt  
         }
         self.client.get(url, headers=headers)
 
@@ -69,7 +67,3 @@ class WebsiteUser(HttpUser):
     @task
     def get_items_list(self):
         self.client.get("/items/list?sort=price&sort_order=asc")
-
-    def user_id(self):
-        # A simple way to generate a unique user ID based on the instance ID
-        return self.environment.runner.user_count
